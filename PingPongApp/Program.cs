5a0952bfb715a57f06a526dadaf00f9968b30f56
nikos <nikos@DESKTOP-F586MH6>
2024-09-03 23:09:57 +0300

    Add PingPongApp

---
+++ PingPongApp/Program.cs
@@ -0,0 +1,30 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Error);
+builder.Logging.AddFilter("Microsoft.AspNetCore.Server.Kestrel", LogLevel.Error);
+
+int portNumber;
+
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!int.TryParse(port, out portNumber))
+{
+    portNumber = 9002;
+}
+
+builder.WebHost.UseKestrel(options =>
+{
+    options.ListenAnyIP(portNumber);
+});
+
+var app = builder.Build();
+
+var counter = 0;
+app.MapGet("/pingpong", () => 
+{
+    return Results.Text($"pong {counter++}");
+});
+
+app.Run();
--- deploy.ps1
+++ deploy.ps1
@@ -5,3 +5,7 @@ docker image push roaringduck/dwk-logoutput:latest
 docker build .\TodoApp\ -t dwk-todoapp
 docker image tag dwk-todoapp:latest roaringduck/dwk-todoapp:latest
 docker image push roaringduck/dwk-todoapp:latest
+
+docker build .\PingPongApp\ -t dwk-pingpongapp
+docker image tag dwk-pingpongapp:latest roaringduck/dwk-pingpongapp:latest
+docker image push roaringduck/dwk-pingpongapp:latest
