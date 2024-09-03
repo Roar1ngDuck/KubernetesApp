36ee6602de5034416a94a11dc49181dda9f18a75
nikos <nikos@DESKTOP-F586MH6>
2024-09-03 22:40:24 +0300

    Add status API endpoint to LogOutput

--- LogOutput/Program.cs
+++ LogOutput/Program.cs
@@ -1,10 +1,42 @@
-﻿using System.Globalization;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Error);
+builder.Logging.AddFilter("Microsoft.AspNetCore.Server.Kestrel", LogLevel.Error);
+
+int portNumber;
+
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!int.TryParse(port, out portNumber))
+{
+    portNumber = 9001;
+}
+
+builder.WebHost.UseKestrel(options =>
+{
+    options.ListenAnyIP(portNumber);
+});
+
+var app = builder.Build();
 
 var randomString = Guid.NewGuid().ToString();
 
-while (true)
+_ = Task.Run(() => 
+{
+   while (true) 
+   {
+        Thread.Sleep(5000);
+        var timestamp = DateTime.Now.ToString("u", CultureInfo.InvariantCulture);
+        Console.WriteLine($"{timestamp}: {randomString}");
+   }
+});
+
+app.MapGet("/api/status", () => 
 {
     var timestamp = DateTime.Now.ToString("u", CultureInfo.InvariantCulture);
-    Console.WriteLine($"{timestamp}: {randomString}");
-    Thread.Sleep(5000);
-}
+
+    return Results.Text($"{timestamp}: {randomString}");
+});
+
+app.Run();
