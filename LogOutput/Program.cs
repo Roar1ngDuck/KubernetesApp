809bdfe36f937266b7deb554a759a460b88b8f90
nikos <nikos@DESKTOP-F586MH6>
2024-09-03 20:07:34 +0300

    Create LogOutput app

---
+++ LogOutput/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+var randomString = Guid.NewGuid().ToString();
+
+while (true)
+{
+    var timestamp = DateTime.Now.ToString("u", CultureInfo.InvariantCulture);
+    Console.WriteLine($"{timestamp}: {randomString}");
+    Thread.Sleep(5000);
+}
