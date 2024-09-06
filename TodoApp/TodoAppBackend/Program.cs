869dab109a581bb4bd735c6f5aad5df156d3c390
nikos <nikos@DESKTOP-F586MH6>
2024-09-06 23:34:26 +0300

    Modify TodoApp to use new backend for fetching and saving todos

---
+++ TodoApp/TodoAppBackend/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Error);
+builder.Logging.AddFilter("Microsoft.AspNetCore.Server.Kestrel", LogLevel.Error);
+
+int portNumber;
+
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!int.TryParse(port, out portNumber))
+{
+    portNumber = 9003;
+}
+
+builder.WebHost.UseKestrel(options =>
+{
+    options.ListenAnyIP(portNumber);
+});
+
+var app = builder.Build();
+
+app.Use((context, next) =>
+{
+    
+    return next();
+});
+
+var todos = new List<string>();
+
+app.MapGet("/todos", () => 
+{
+    return Results.Ok(todos);
+});
+
+app.MapPost("/todos", async (HttpContext httpContext) => 
+{
+    var form = await httpContext.Request.ReadFormAsync();
+    var todo = form["todo"].ToString();
+    todos.Add(todo);
+    return Results.Redirect("/");
+});
+
+app.Run();
--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,13 @@ docker build ./LogOutput/LogOutputReader -t dwk-logoutput-reader
 docker image tag dwk-logoutput-reader:latest roaringduck/dwk-logoutput-reader:latest
 docker image push roaringduck/dwk-logoutput-reader:latest
 
-docker build ./TodoApp/ -t dwk-todoapp
-docker image tag dwk-todoapp:latest roaringduck/dwk-todoapp:latest
-docker image push roaringduck/dwk-todoapp:latest
+docker build ./TodoApp/TodoAppFrontend/ -t dwk-todoapp-frontend
+docker image tag dwk-todoapp-frontend:latest roaringduck/dwk-todoapp-frontend:latest
+docker image push roaringduck/dwk-todoapp-frontend:latest
+
+docker build ./TodoApp/TodoAppBackend/ -t dwk-todoapp-backend
+docker image tag dwk-todoapp-backend:latest roaringduck/dwk-todoapp-backend:latest
+docker image push roaringduck/dwk-todoapp-backend:latest
 
 docker build ./PingPongApp/ -t dwk-pingpongapp
 docker image tag dwk-pingpongapp:latest roaringduck/dwk-pingpongapp:latest
