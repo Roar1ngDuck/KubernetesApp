1ceebb2f91499fd9c818c038354e72cb34c8f577
nikos <nikos@DESKTOP-F586MH6>
2024-09-20 12:07:51 +0300

    Update .gitignore

--- PingPongApp/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build ./PingPongApp/ -t dwk-pingpongapp
-docker image tag dwk-pingpongapp:latest roaringduck/dwk-pingpongapp:latest
-docker image push roaringduck/dwk-pingpongapp:latest
